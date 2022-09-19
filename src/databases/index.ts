3b834b1c84a4f11443aacb475aee588fd7e0dcb0
Hanleyjames <hanley.doth@outlook.com>
2022-09-19 11:56:58 -0600

    Add and updates
    Add env files to git ignore besides dev, add outline for working dev env var without any meaningful secrets. Update packages to allow Mikro-orm to work with mikro orm cli. Update Config to take new user and password env var, and update database config to use new username and password vars.

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,11 +1,14 @@
 import { EntityManager, EntityRepository, MikroORM, Options } from '@mikro-orm/core';
-import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { NODE_ENV, DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } from '@config';
 import { BaseEntity } from '@entities/base.entity';
 import { UserEntity } from '@entities/users.entity';
+import { env } from 'process';
 
 export const dbOptions: Options = {
   type: 'postgresql',
   clientUrl: `postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  user: `${DB_USER}`,
+  password: `${DB_PASSWORD}`,
   entities: [BaseEntity, UserEntity],
   debug: NODE_ENV === 'development' ? true : false,
 };
